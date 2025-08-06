4f0b583a03c2128afa0a2a50200d592ce380f0fc
younggyu01 <gugy0706@gmail.com>
2025-08-06 15:21:22 +0900

    Account 객체 생성

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,37 @@
+package workshop.account.control;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		//1. Account 객체 생성 - 기본 생성자 호출
+		Account account = new Account();
+		
+		//1-1. setter 메서드 호출로 값을 대입
+		// 고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100
+		account.setCustId("A1100");
+		account.setAcctId("221-22-3477");
+		account.deposit(1000);
+		
+		//1-2. getter 메서드 호출로 값을 확인
+		System.out.println("고객번호 = " + account.getCustId());
+		System.out.println("계좌번호 = " + account.getAcctId());
+		System.out.println("잔액 = " + account.getBalance());
+		
+		//2. Account 객체생성 - 오버로딩된 생성자 호출
+		Account account2 = new Account("B1200", "331-22-3477", 2000);
+		System.out.println("고객번호 = " + account2.getCustId());
+		System.out.println("계좌번호 = " + account2.getAcctId());
+		System.out.println("잔액 = "    + account2.getBalance());
+		
+		System.out.println("10000원 입금");
+		account2.deposit(10000);
+		System.out.println("잔액 = "    + account2.getBalance());
+		
+		System.out.println("10000원 출금");
+		account2.withdraw(10000);
+		System.out.println("잔액 = "    + account2.getBalance());
+	}
+
+}
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,55 @@
 package workshop.account.entity;
 
 public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
 
+	// default constructor 선언
+	public Account() {
+		System.out.println("기본생성자 호출됨");
+	}
+
+	// Constructor Overloading (생성자 중복 정의)
+	public Account(String custId, String acctId, int balance) {
+//		this.custId = custId;
+//		this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
+		this.balance = balance;
+	}
+
+//	public void setBlance(int balance) {
+//		this.balance = balance;
+//	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+
+	public String getAcctId() {
+		return acctId;
+	}
+
+	// 입금
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+
+	// 출금
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	}
 }
