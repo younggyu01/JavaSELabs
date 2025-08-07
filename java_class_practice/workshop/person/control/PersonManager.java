9544bf8f0ba8b1aa14c9da8b53ac48c617096cc6
younggyu01 <gugy0706@gmail.com>
2025-08-07 10:12:10 +0900

    수업중 실습2

---
+++ java_class_practice/workshop/person/control/PersonManager.java
@@ -0,0 +1,59 @@
+package workshop.person.control;
+
+import workshop.person.entity.PersonEntity;
+
+public class PersonManager {
+
+	public static void main(String[] args) {
+		PersonManager personMgr = new PersonManager();
+		
+		personMgr.printTitle("인물정보 조회시스템");
+		
+		//배열선언 및 초기화
+		PersonEntity[] persons = new PersonEntity[10];
+		//persons 변수는 PersonEntity[] 타입이고, persons[0]은 PersonEntity 타입이다.
+		personMgr.fillPersons(persons);
+		
+		personMgr.showPerson(persons);
+		
+		char gender = '남';
+		String message = String.format("성별: %s (은)는  %d 명 입니다.", gender, personMgr.findByGender(persons, gender));
+		System.out.println(message);
+	}
+	
+	public int findByGender(PersonEntity[] persons, char gender ) {
+		int genderCnt = 0;
+		for(PersonEntity person : persons) {
+			if(person.getGender() == gender) {
+				genderCnt++;
+			}
+		}
+		return genderCnt;
+	}
+
+	public void showPerson(PersonEntity[] persons) {
+		//for loop를 순회하면서 
+		for(PersonEntity person:persons) {
+			System.out.println("[이름 ] " + person.getName() + "\t [성별] " + person.getGender() + "\t [전화번호] " + person.getPhone());
+		}
+	}
+
+	public void fillPersons(PersonEntity[] persons) {
+		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
+		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
+		persons[2] = new PersonEntity("박영수","7503111233201", "서울 성북구", "02-887-1542");
+		persons[3] = new PersonEntity("나인수","7312041038988", "대전 유성구", "032-384-2223");
+		persons[4] = new PersonEntity("홍정수","7606221021341", "서울 양천구", "02-158-7333");
+		persons[5] = new PersonEntity("이미숙","7502142021321", "서울 강서구", "02-323-1934");
+		persons[6] = new PersonEntity("박성구","7402061023101", "서울 종로구", "02-308-0932");
+		persons[7] = new PersonEntity("유성미","7103282025101", "서울 은평구", "02-452-0939");
+		persons[8] = new PersonEntity("황재현","7806231031101", "인천 중구", "032-327-2202");
+		persons[9] = new PersonEntity("최철수","7601211025101", "인천 계양구", "032-122-7832");
+	}
+	
+	public void printTitle(String title) {
+		System.out.println("@@@@ " + title + " @@@@");
+		System.out.println();
+	}
+
+}
---
+++ java_class_practice/workshop/person/entity/PersonEntity.java
@@ -0,0 +1,72 @@
+package workshop.person.entity;
+
+public class PersonEntity {
+	private String name;
+	private char gender;
+	private String ssn;
+	private String address;
+	private String phone;
+	
+	//default constructor
+	public PersonEntity() {
+		
+	}
+
+	//overloading constructor
+	public PersonEntity(String name, String ssn, String address, String phone) {
+		this.name = name;
+		//this.ssn = ssn;
+		setSsn(ssn);
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+		if(ssn != null) {
+			char genderNum = ssn.charAt(6);
+			if(genderNum == '1' || genderNum == '3') {
+				setGender('남');
+			}else {
+				setGender('여');
+			}
+		}
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}	
+	
+}
